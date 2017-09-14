bab9fef585f93fff61e2bced2585b9bcb4dd4087
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-14 18:56:59 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,7 @@ function submitIt() {
 }
 
 function pressIt() {
-  $('document').on('keydown', function(e) {
+  $('body').on('keydown', function(e) {
     if (e.which == 71) {
       alert('g was pressed')
     }
