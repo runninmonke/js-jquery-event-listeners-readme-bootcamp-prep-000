598b4e2a853bd7dfebad49fdd3b7272c70404306
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-14 18:37:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,26 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(e){
+    console.log(e)
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function pressIt() {
+  $('document').on('keydown', function(e) {
+    console.log(e.which || e.detail);
+  })
+}
 
 $(document).ready(function(){
 
