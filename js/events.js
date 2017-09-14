edb2a92be7bf3276f855c477e91871dbe621de1b
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-14 18:57:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(e){
+    e.target.className = 'tasty';
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(e) {
+    if (e.which == 71) {
+      alert('g was pressed')
+    }
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  pressIt();
+  submitIt();
+  frameIt();
+  getIt();
 });
