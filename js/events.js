73b92e53266ed729a2d4a0a054b541de8a9de621
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-14 18:39:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,6 +7,7 @@ function getIt() {
 function frameIt() {
   $('img').on('load', function(e){
     console.log(e)
+    debugger
   })
 }
 
@@ -19,6 +20,7 @@ function submitIt() {
 function pressIt() {
   $('document').on('keydown', function(e) {
     console.log(e.which || e.detail);
+    debugger
   })
 }
 
